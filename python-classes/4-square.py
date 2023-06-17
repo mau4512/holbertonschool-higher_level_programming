49b47b227e3c58b8feb02862b8c21bb7ba02aed8
mau4512 <mauricio.25.17@gmail.com>
2023-06-17 07:53:26 -0700

    pushing corrections

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-
+"""Define a class Square."""
 class Square:
+    """Represent a square."""
     def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
 
     @property
     def size(self):
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -18,4 +25,5 @@ class Square:
             self.__size = value
 
     def area(self):
+        """Return the current area of the square."""
         return (self.__size ** 2)
