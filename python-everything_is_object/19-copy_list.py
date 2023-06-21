483b4fcc2db55168e2f197ce5ab0a478cfa848f7
mau4512 <mauricio.25.17@gmail.com>
2023-06-21 15:14:08 -0700

    making corrections

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def copy_list(a_list):
-    new_list = []
-    for index in range(len(a_list)):
-        new_list.append(a_list[index])
-    return (new_list)
-        
+    return a_list.copy()
