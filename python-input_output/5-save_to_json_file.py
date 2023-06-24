71eed69f5ca6b172d7bd6c05d74d4e99c68bc6db
mau4512 <mauricio.25.17@gmail.com>
2023-06-23 18:44:39 -0700

    pushing exercise 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a JSON file-writing function"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation
+    Args:
+        my_obj: object to write to file
+        filename: file to write the object
+    """
+    with open(filename, "w") as f:
+        json.dump(my_obj,f)
