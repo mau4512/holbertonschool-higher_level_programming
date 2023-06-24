d1de6ea1730db561b08acf2e7b63f2d720414def
mau4512 <mauricio.25.17@gmail.com>
2023-06-23 20:57:32 -0700

    pushin exercesis

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -4,7 +4,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represnet a square"""
+    """Represent a square"""
 
     def __init__(self, size):
         """Initialize a new square
