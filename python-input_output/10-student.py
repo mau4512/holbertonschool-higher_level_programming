a3ea6709356afb16e8517adeee1304cc871125c9
mau4512 <mauricio.25.17@gmail.com>
2023-06-23 20:24:03 -0700

    excersise 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -15,7 +15,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
 
@@ -24,7 +24,7 @@ class Student:
         Args:
             attrs: (Optional) The attributes to represent.
         """
-        if (type(attrs) == list and 
+        if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
