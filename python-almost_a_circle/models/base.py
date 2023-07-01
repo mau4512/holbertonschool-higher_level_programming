111f93edffacfe2d0e9b5be0cd019ce4f59cbe9e
mau4512 <mauricio.25.17@gmail.com>
2023-07-01 15:54:47 -0700

    adding

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -40,7 +40,7 @@ class Base:
         filename = cls.__name__ + ".json"
         with open(filename, "w") as jsonfile:
             if list_objs is None:
-                jsonFile.write("[]")
+                jsonfile.write("[]")
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 jsonfile.write(Base.to_json_string(list_dicts))
