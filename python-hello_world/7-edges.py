ca06c67e15ca31b341e1c7538160140049917a9e
mau4512 <mauricio.25.17@gmail.com>
2024-01-08 00:39:47 +0000

    pushing excersises

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = "{}".format(word[:3]) # empty line in provided script
+word_last_2 = "{}".format(word[-2:]) # empty line in provided script
+middle_word = "{}".format(word[1:-1]) # empty line in provided script
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
